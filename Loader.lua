e4046baf37eb5849ed01ecc08be2ce637f1d4718
TheGreatOath <pertuisance@gmail.com>
2025-04-13 19:16:21 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,21 +5,20 @@ local API = loadstring(game:HttpGet("https://sdkAPI-public.luarmor.net/library.l
 local pastebinUrl = "https://pastebin.com/raw/DkyEE1m7"
 local staticKey = game:HttpGet(pastebinUrl)
 if game.placeId == 8075399143 then
-    API.script_id = "38bc18fab08ad4249ddfb77f69dbff8d"
+
 elseif game.placeId == 9164271501 then
-    API.script_id = "ef9455c5229388090b6d981daf114ff8"
+
 elseif game.placeId == 18668065416 then
-    API.script_id = "ff5cdf86ffe96e84228656716cf17fb2"
-	elseif game.placeId == 92517437168342 then
-    API.script_id = "ff5cdf86ffe96e84228656716cf17fb2"
-	elseif game.placeId == 115110570222234 then
-    API.script_id = "ff5cdf86ffe96e84228656716cf17fb2"
+
+elseif game.placeId == 92517437168342 then
+
+elseif game.placeId == 115110570222234 then
+
 elseif game.placeId == 91797414023830 then
-	API.script_id = "710ce19e67b940f222204ab442ec469f"
+
 elseif game.placeId == 140050746237057 then
-	API.script_id = "710ce19e67b940f222204ab442ec469f"
+
 elseif game.placeId == 89413197677760 then
-	API.script_id = "9c30ffc7ec5776d9e1149c6e4ff80b12"
 else
     game.Players.LocalPlayer:Kick("Worm doesn't support this game | Join our discord for more information")
 end

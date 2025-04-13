622325ee0acc8e004464605147125cdd512305e0
TheGreatOath <pertuisance@gmail.com>
2025-04-13 19:18:42 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,21 +3,9 @@ repeat task.wait() until game:IsLoaded()
 local UI = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local pastebinUrl = "https://pastebin.com/raw/DkyEE1m7"
 local staticKey = game:HttpGet(pastebinUrl)
-if game.placeId == 8075399143 then
+if game.placeId == 72368345999613 then
 
-elseif game.placeId == 9164271501 then
-
-elseif game.placeId == 18668065416 then
-
-elseif game.placeId == 92517437168342 then
-
-elseif game.placeId == 115110570222234 then
-
-elseif game.placeId == 91797414023830 then
-
-elseif game.placeId == 140050746237057 then
-
-elseif game.placeId == 89413197677760 then
+elseif game.placeId == 4483381587 then
 else
     game.Players.LocalPlayer:Kick("Worm doesn't support this game | Join our discord for more information")
 end

916681ba54f31a6ccffed45086bd395fa3022e0f
TheGreatOath <pertuisance@gmail.com>
2025-05-08 21:53:44 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,6 +1,6 @@
 local Games = {
   [4878988249] = "https://raw.githubusercontent.com/TheGreatOath/Script-Games/refs/heads/main/Migty%20Omega.lua",
-  [4483381587] = "https://raw.githubusercontent.com/TheGreatOath/Script-Games/refs/heads/main/Migty%20Omega.lua",
+  [4483381587] = print("Hello PAPAPA"),
 }
 
 return Games

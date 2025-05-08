7eef0c10c37948b35b164a59babac63c616d323f
TheGreatOath <pertuisance@gmail.com>
2025-05-08 19:59:33 +0300

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'Settings'
+	SaveManager.Folder = ''
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {

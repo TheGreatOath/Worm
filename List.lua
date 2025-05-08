dcda139713efa4d0872ce1fa89c918ec171fab3f
TheGreatOath <pertuisance@gmail.com>
2025-05-08 19:16:57 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,6 +1,6 @@
 local Games = {
   [4878988249] = "https://raw.githubusercontent.com/TheGreatOath/Lib/refs/heads/main/Migty%20Omega.lua", -- Mighty Omega
-  [4483381587] = "https://raw.githubusercontent.com/TheGreatOath/Lib/refs/heads/main/Migty%20Omega.lua"
+  [4483381587] = "https://raw.githubusercontent.com/TheGreatOath/Lib/refs/heads/main/Migty%20Omega.lua",
 }
 
 return Games

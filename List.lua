f98514abb8b31a17011bcbb57757bdbaf7222b22
TheGreatOath <pertuisance@gmail.com>
2025-05-08 19:34:07 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -3,4 +3,7 @@ local Games = {
   [4483381587] = "https://raw.githubusercontent.com/TheGreatOath/Script-Games/refs/heads/main/Migty%20Omega.lua",
 }
 
-return Games
+if not Games[game.placeId] then
+    Players.LocalPlayer:Kick("Worm doesn't support this game | Join our discord for more information")
+    return
+end

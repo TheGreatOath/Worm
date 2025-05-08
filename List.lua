bbd29c2a2326e279bf5c1ba60325e51c1cda180c
TheGreatOath <pertuisance@gmail.com>
2025-05-08 19:39:55 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,9 +1,6 @@
 local Games = {
-  [4878988249] = "https://raw.githubusercontent.com/TheGreatOath/Script-Games/refs/heads/main/Migty%20Omega.lua", -- Mighty Omega
+  [4878988249] = "https://raw.githubusercontent.com/TheGreatOath/Script-Games/refs/heads/main/Migty%20Omega.lua",
   [4483381587] = "https://raw.githubusercontent.com/TheGreatOath/Script-Games/refs/heads/main/Migty%20Omega.lua",
 }
 
-if not Games[game.placeId] then
-    Players.LocalPlayer:Kick("Worm doesn't support this game | Join our discord for more information")
-    return
-end
+return Games
